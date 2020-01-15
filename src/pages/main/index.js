7a0534e4ba7744d66b139443b211bf86fc05a86b
danieljpgo <djpgonzaga@gmail.com>
2020-01-15 11:52:50 -0300

    pagination

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,26 +1,56 @@
 import React, {Component} from 'react';
 import api from '../../services/api';
+import './styles.css';
 
 export default class Main extends Component {
     state = {
-        texts: []
+        texts: [],
+        textCount: {},
+        page: 1
     };
 
     componentDidMount() {
         this.loadData();
     }
 
-    loadData = async () => {
-        const response = await api.get('/vocabularies');
-        this.setState({texts: response.data.rows})
+    loadData = async (page = 1) => {
+        const response = await api.get(`/vocabularies?page=${page}`);
+
+        console.log(response)
+
+        const { count, rows } = response.data;
+
+        this.setState({texts: rows, textCount: count, page: page})
+    };
+
+    prevPage = () => {
+        const { page } = this.state;
+
+        if (page === 1) return;
+
+        const pageNumber = page - 1;
+        this.loadData(pageNumber);
+    };
+
+    nextPage = () => {
+        const { page } = this.state;
+
+        const pageNumber = page + 1;
+        this.loadData(pageNumber);
     };
 
     render() {
+        const { texts, page } = this.state;
+
         return (
             <div className="texts-list">
                 {this.state.texts.map(text => (
-                    <h2 key={text?.id}>{text?.text}</h2>
+                    <div key={text?.id}>{text?.text}</div>
                 ))}
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button onClick={this.nextPage}>Proximo</button>
+                </div>
             </div>
         )
     }
